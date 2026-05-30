bd990b754fe56d6d3853ba8c82f8e636fd76d146
Sachin Som <sachin.som@celebaltech.com>
2026-05-30 18:22:58 +0000

    fix: remove unsupported subnet tags
    Co-authored-by: Sachin Som <sachin.som@celebaltech.com>

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -45,5 +45,4 @@ resource "azurerm_subnet" "segments" {
   resource_group_name  = azurerm_resource_group.vnet.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = [each.value.cidr]
-  tags                 = local.common_tags
 }
